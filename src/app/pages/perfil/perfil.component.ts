d4070629032a096045a7dd1b00280fe9945fae14
edsonsilvajr <edsonsilvajr21@gmail.com>
2023-02-08 21:32:53 -0300

    storing repositories in the component

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './../../services/data.service';
+import { GithubService } from './../../services/github.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.css']
+  styleUrls: ['./perfil.component.css'],
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
+  private _userData: any; //TODO: criar modelo
+  private _userRepos: any; //TODO: criar modelo
 
+  constructor(
+    private _dataService: DataService,
+    private _gitService: GithubService
+  ) {}
+
+  ngOnInit() {
+    this._userData = this._dataService.getData();
+    this._gitService.getUserRepos(this._userData?.login).subscribe({
+      next: (repos) => {
+        console.log(repos);
+        this._userRepos = repos;
+      },
+      error: () => {},
+    });
+  }
+
+  get userData() {
+    return this._userData;
+  }
+
+  get userRepos() {
+    return this._userRepos;
+  }
 }
