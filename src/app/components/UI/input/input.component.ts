36a93eb73107bdbf70dcef4894b23ee5f108568c
edsonsilvajr <edsonsilvajr21@gmail.com>
2023-02-09 16:19:45 -0300

    changing input logic

--- src/app/components/UI/input/input.component.ts
+++ src/app/components/UI/input/input.component.ts
@@ -1,10 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css'],
 })
-export class InputComponent {
-  public value = '';
+export class InputComponent implements OnInit {
+  @ViewChild('input') inputElement: ElementRef | undefined;
+
+  @Input() public value = '';
+
+  ngOnInit() {
+    this.value = this.inputElement?.nativeElement.value;
+  }
 }
