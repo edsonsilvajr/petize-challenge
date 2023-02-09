68e094386ab35581b236d399da0ae6bd3b188920
edsonsilvajr <edsonsilvajr21@gmail.com>
2023-02-09 15:32:07 -0300

    repo card component created

--- src/app/components/repo-card/repo-card.component.ts
+++ src/app/components/repo-card/repo-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { UserRepo } from './../../models/userRepo.model';
 
 @Component({
   selector: 'app-repo-card',
@@ -6,5 +7,17 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./repo-card.component.css'],
 })
 export class RepoCardComponent {
-  @Input() repo: any;
+  @Input() repo: UserRepo | null = null;
+  public timeDiff = '';
+
+  ngOnChanges() {
+    if (this.repo === null) return;
+    const date = new Date(this.repo.updated_at);
+    const now = new Date();
+    const diff = Math.floor(
+      (now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24)
+    );
+
+    this.timeDiff = diff > 1 ? 'há ' + diff + ' dia(s)' : 'recentemente';
+  }
 }
