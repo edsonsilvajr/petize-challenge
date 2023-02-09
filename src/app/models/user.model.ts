858676c30c0266c8c76cecdb212ffafd73e2d2f0
edsonsilvajr <edsonsilvajr21@gmail.com>
2023-02-09 04:38:26 -0300

    user model defined

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,13 @@
+export interface User {
+  avatar_url: string | null;
+  bio: string | null;
+  company: string | null;
+  email: string | null;
+  login: string | null;
+  followers: string | null;
+  following: string | null;
+  name: string | null;
+  twitter_username: string | null;
+  location: string | null;
+  blog: string | null;
+}
