a9114cec0228c5284da84b0b18e16f007dc99819
edsonsilvajr <edsonsilvajr21@gmail.com>
2023-02-08 21:42:11 -0300

    receiving data via Input() on both card components

--- src/app/components/repo-card/repo-card.component.ts
+++ src/app/components/repo-card/repo-card.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-repo-card',
   templateUrl: './repo-card.component.html',
-  styleUrls: ['./repo-card.component.css']
+  styleUrls: ['./repo-card.component.css'],
 })
 export class RepoCardComponent {
-
+  @Input() repo: any;
 }
--- src/app/components/user-card-info/user-card-info.component.ts
+++ src/app/components/user-card-info/user-card-info.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-card-info',
   templateUrl: './user-card-info.component.html',
-  styleUrls: ['./user-card-info.component.css']
+  styleUrls: ['./user-card-info.component.css'],
 })
 export class UserCardInfoComponent {
-
+  @Input() data: any;
 }
