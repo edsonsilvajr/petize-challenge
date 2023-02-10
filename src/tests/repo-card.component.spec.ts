cbfe7f952db3fbdc3f52e705931d34d7bfda8b0b
edsonsilvajr <edsonsilvajr21@gmail.com>
2023-02-10 03:17:11 -0300

    repo card component tests

--- src/tests/repo-card.component.spec.ts
+++ src/tests/repo-card.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UserRepo } from 'src/app/models/userRepo.model';
 
 import { RepoCardComponent } from '../app/components/repo-card/repo-card.component';
 
@@ -18,5 +19,24 @@ describe('RepoCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the timeDiff property to "recentemente" when the updated_at date is recent', () => {
+    component.repo = {
+      updated_at: new Date().toISOString(),
+    } as UserRepo;
+    component.ngOnChanges();
+    expect(component.timeDiff).toEqual('recentemente');
+  });
+
+  it('should set the timeDiff property to "há X dia(s)" when the updated_at date is X days ago', () => {
+    const daysAgo = 10;
+    component.repo = {
+      updated_at: new Date(
+        Date.now() - daysAgo * 24 * 60 * 60 * 1000
+      ).toISOString(),
+    } as UserRepo;
+    component.ngOnChanges();
+    expect(component.timeDiff).toEqual(`há ${daysAgo} dia(s)`);
   });
 });
