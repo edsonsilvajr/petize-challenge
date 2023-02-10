3480a0cab6325cf25d71831de2fc949df95a815b
edsonsilvajr <edsonsilvajr21@gmail.com>
2023-02-10 02:29:37 -0300

    input component tests

--- src/tests/input.component.spec.ts
+++ src/tests/input.component.spec.ts
@@ -1,22 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { InputComponent } from '../app/components/UI/input/input.component';
+import { InputComponent } from 'src/app/components/UI/input/input.component';
+import { TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('InputComponent', () => {
   let component: InputComponent;
-  let fixture: ComponentFixture<InputComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [InputComponent],
     }).compileComponents();
+  });
 
-    fixture = TestBed.createComponent(InputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    component = new InputComponent();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('keyupListener', () => {
+    it('should emit the event if key is Enter', () => {
+      spyOn(component.enterKeyEvent, 'next');
+      component.keyupListener({
+        key: 'Enter',
+      } as KeyboardEvent);
+      expect(component.enterKeyEvent.next).toHaveBeenCalledWith(true);
+    });
+
+    it('should not emit the event if key is not Enter', () => {
+      spyOn(component.enterKeyEvent, 'next');
+      component.keyupListener({
+        key: 'Other key',
+      } as KeyboardEvent);
+      expect(component.enterKeyEvent.next).not.toHaveBeenCalled();
+    });
   });
 });
